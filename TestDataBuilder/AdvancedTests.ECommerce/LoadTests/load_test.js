d3285c13b9862fbd15977ec283461a5a517c0b8b
Igor Gomes <igor.gomes@windifferent.com>
2025-05-12 10:33:29 -0300

    5.5, 5.6 - stages and checks

--- TestDataBuilder/AdvancedTests.ECommerce/LoadTests/load_test.js
+++ TestDataBuilder/AdvancedTests.ECommerce/LoadTests/load_test.js
@@ -1,16 +1,23 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 
 const BASE_URL = 'http://localhost:5000';
 
 export const options = {
-    // iterations: 10,
-    vus: 5,
-    duration: '20s'
+    stages: [
+        { duration: '30s', target: 100 },  // Ramp-up: 0 → 100 VUs em 30s
+        { duration: '1m', target: 100 },   // Carga sustentada
+        { duration: '30s', target: 200 },  // Aumento da acesso
+        { duration: '1m', target: 200 },   // Estabilização
+        { duration: '1m', target: 0 },     // Ramp-down
+    ]
 };
 
 export default () => {
-    http.get(BASE_URL + '/orders?customerId=1');
-
+    const res = http.get(BASE_URL + '/orders?customerId=50');
+    check(res, {
+        'is status 200': (r) => r.status === 200,
+        'result is not empty list': (r) => r.body.length > 2
+    });
     sleep(1);
 }

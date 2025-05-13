988b31586df916703bf2b99faec0ce74c0a510c2
Igor Gomes <igor.gomes@windifferent.com>
2025-05-13 11:09:30 -0300

    5.7 - Setup and Teardown

--- TestDataBuilder/AdvancedTests.ECommerce/LoadTests/load_test.js
+++ TestDataBuilder/AdvancedTests.ECommerce/LoadTests/load_test.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+import exec from 'k6/execution';
 
 const BASE_URL = 'http://localhost:5000';
 
@@ -13,11 +14,26 @@ export const options = {
     ]
 };
 
-export default () => {
-    const res = http.get(BASE_URL + '/orders?customerId=50');
+export function setup() {
+    return { token: 'jwt-token', payload: [...Array(1000).keys()].map(x => x + 1) };
+}
+
+export default (data) => {
+    const params = {
+        headers: {
+            'Authorization': 'Bearer ' + data.token,
+        },
+    };
+    const id = data.payload[exec.vu.iterationInInstance % data.payload.length];
+    console.log(id);
+    const res = http.get(BASE_URL + `/orders?customerId=${id}`, params);
     check(res, {
         'is status 200': (r) => r.status === 200,
         'result is not empty list': (r) => r.body.length > 2
     });
     sleep(1);
+}
+
+export function teardown(data) {
+    console.log('TearDown: ', data);
 }

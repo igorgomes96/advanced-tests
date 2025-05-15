04499ca5c6efc17278880b0d2d16b03b38598f97
Igor Gomes <igor.gomes@windifferent.com>
2025-05-15 12:07:41 -0300

    5.9 - Scenarios

--- TestDataBuilder/AdvancedTests.ECommerce/LoadTests/load_test.js
+++ TestDataBuilder/AdvancedTests.ECommerce/LoadTests/load_test.js
@@ -8,13 +8,36 @@ const BASE_URL = 'http://localhost:5000';
 const payload = new SharedArray('payload', () => [...Array(1000).keys()].map(x => x + 1));
 
 export const options = {
-    stages: [
-        { duration: '30s', target: 100 },  // Ramp-up: 0 → 100 VUs em 30s
-        { duration: '1m', target: 100 },   // Carga sustentada
-        { duration: '30s', target: 200 },  // Aumento da acesso
-        { duration: '1m', target: 200 },   // Estabilização
-        { duration: '1m', target: 0 },     // Ramp-down
-    ]
+    scenarios: {
+        sharedIterations: {
+            executor: 'shared-iterations',
+            vus: 10,
+            iterations: 50,
+            maxDuration: '15s'
+        },
+        perVuIterations: {
+            executor: 'per-vu-iterations',
+            vus: 10,
+            iterations: 10,
+            maxDuration: '25s',
+            startTime: '5s'
+        },
+        constantVus: {
+            executor: 'constant-vus',
+            vus: 10,
+            duration: '10s',
+            startTime: '10s'
+        },
+        rampingVus: {
+            executor: 'ramping-vus',
+            startTime: '20s',
+            stages: [
+                { duration: '15s', target: 100 },  // Ramp-up: 0 → 100 VUs em 30s
+                { duration: '30s', target: 100 },   // Carga sustentada
+                { duration: '15s', target: 0 },     // Ramp-down
+            ]
+        }
+    }
 };
 
 export function setup() {
@@ -28,7 +51,6 @@ export default (data) => {
         },
     };
     const id = payload[exec.vu.iterationInInstance % payload.length];
-    console.log(id);
     const res = http.get(BASE_URL + `/orders?customerId=${id}`, params);
     check(res, {
         'is status 200': (r) => r.status === 200,
